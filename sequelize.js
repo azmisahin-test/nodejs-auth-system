36960584b6027182f560e2646e1394e827b8cbde
Azmi SAHIN <azmisahin@outlook.com>
2024-07-20 20:37:18 +0300

    orm

---
+++ sequelize.js
@@ -0,0 +1,50 @@
+const { Sequelize, DataTypes } = require("sequelize");
+require("dotenv").config();
+const createLogger = require("./logger");
+const logger = createLogger("ORM");
+
+// Sequelize instance
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  // logging: logger.info.bind(logger) // Winston ile loglama
+  logging: false, // SQL sorgularını loglamayı kapatır
+});
+
+// Test connection
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info("Connection has been established successfully.");
+  } catch (error) {
+    logger.error("Unable to connect to the database:", error);
+  }
+};
+
+// Define User model
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  permissions: {
+    type: DataTypes.JSONB,
+    allowNull: false,
+  },
+});
+
+// Sync models
+const syncModels = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    logger.info("Database & tables created!");
+  } catch (error) {
+    logger.error("Error syncing models:", error);
+  }
+};
+
+module.exports = { sequelize, testConnection, syncModels, User };
