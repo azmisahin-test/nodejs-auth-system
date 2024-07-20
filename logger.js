cbb9f4ff7fd0e01231c4e796d612d75b6dde0fc9
Azmi SAHIN <azmisahin@outlook.com>
2024-07-20 20:35:56 +0300

    logger

---
+++ logger.js
@@ -0,0 +1,31 @@
+const winston = require("winston");
+require("dotenv").config();
+const fs = require("fs");
+
+// Log dizinini oluşturun (varsa) veya oluşturun
+const logDirectory = process.env.LOG_DIR || "logs";
+const filename = process.env.LOG_FILE || "combined.log";
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+// Logger konfigürasyonu
+const createLogger = (moduleName) => {
+  const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }) => {
+        return `${timestamp} ${level.toUpperCase()} [${moduleName}]: ${message}`;
+      })
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: `${logDirectory}/${filename}` }),
+    ],
+  });
+
+  return logger;
+};
+
+module.exports = createLogger;
